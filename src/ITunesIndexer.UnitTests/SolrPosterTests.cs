bebd12470bc0b55f47e1d6ef86c3968071136f3d
Greg Sochanik <greg@sochanik.com>
2010-11-12 23:27:42 +0000

    Split tests into integration and uni

---
+++ src/ITunesIndexer.UnitTests/SolrPosterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace ITunesIndexer.UnitTests
+{
+    public class SolrPosterTests
+    {
+        [Test]
+        public void Item_can_convert_to_vald_xml()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Can_creat_http_post_of_item()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void Item_is_added_to_solr_instance()
+        {
+        }
+    }
+}
---
+++ src/ITunesIndexer/SolrPoster.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ITunesIndexer
+{
+    public class SolrPoster<T> : ISolrPoster<T>
+    {
+        // COnvert SOng to XMl
+        // cretae http post
+        // post song xml to solr
+    }
+
+
+    public interface ISolrPoster<T>
+    {
+    }
+}
