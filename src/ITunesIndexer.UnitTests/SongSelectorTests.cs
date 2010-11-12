77a40791a5f0e7d2cf4a86ac062898c0162ec519
Greg Sochanik <greg@sochanik.com>
2010-11-12 09:05:08 +0000

    Setup proper tests for SongSelector - 100% coverage

---
+++ src/ITunesIndexer.UnitTests/SongSelectorTests.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ITunesIndexer.Models;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace ITunesIndexer.UnitTests
+{
+    public class SongSelectorTests
+    {
+        private ILibraryBuilder<Song> _libraryBuilderStub;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _libraryBuilderStub = MockRepository.GenerateStub<ILibraryBuilder<Song>>();
+        }
+
+
+        [Test]
+        public void Should_return_1_song()
+        {
+            const string expectedSong = "Be Somebody";
+            
+            Given_a_library_builder_with_multiple_songs();
+
+            var selector = new SongSelector(_libraryBuilderStub);
+            Song song = selector.FindSingle(x => x.Name == expectedSong);
+
+            Assert.IsNotNull(song);
+            Assert.AreEqual(expectedSong, song.Name);
+        }
+
+        private void Given_a_library_builder_with_multiple_songs()
+        {
+            var stubbedSongs = TestData();
+            _libraryBuilderStub.Stub(x => x.BuildLibrary(Arg<string>.Is.Anything)).Return(stubbedSongs);
+        }
+
+        [Test]
+        public void Should_return_many_songs()
+        {
+            const string expected = "Interpol";
+
+            Given_a_library_builder_with_multiple_songs();
+
+            var selector = new SongSelector(_libraryBuilderStub);
+            IEnumerable<Song> songs = selector.FindMany(x => x.Artist == "Interpol");
+
+            Assert.IsNotEmpty(songs.ToList());
+            
+            Song first = songs.First();
+            
+            Assert.IsNotNull(first);
+            Assert.AreEqual(expected, first.Artist);
+        }
+
+        [Test]
+        public void Should_return_null_if_no_songs_found()
+        {
+            Given_a_library_builder_with_no_songs();
+
+            var selector = new SongSelector(_libraryBuilderStub);
+
+            Song single = selector.FindSingle(x => x.Artist=="Never found");
+
+            Assert.IsNull(single);
+        }
+
+        private void Given_a_library_builder_with_no_songs()
+        {
+            var stubbedSongs = new List<Song>();
+            _libraryBuilderStub.Stub(x => x.BuildLibrary(Arg<string>.Is.Anything)).Return(stubbedSongs);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void Should_return_1_song_based_on_name()
+        {
+            var librarybuilder = new LibraryBuilder<Song>();
+            var selector = new SongSelector(librarybuilder);
+
+            Song song = selector.FindSingle(x => x.Name == "Come Ray And Come Charles");
+
+            Assert.IsNotNull(song);
+            Assert.AreEqual("Come Ray And Come Charles", song.Name);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void Should_return_songs_based_on_bitrate()
+        {
+            var librarybuilder = new LibraryBuilder<Song>();
+            var selector = new SongSelector(librarybuilder);
+
+            IEnumerable<Song> songs = selector.FindMany(x => x.BitRate > 40000);
+            IEnumerable<Song> allSongs = librarybuilder.BuildLibrary(ConfigSettings.PathToXml);
+
+            Assert.Greater(1, songs.Count());
+
+            Assert.Less(songs.Count(), allSongs.Count());
+        }
+
+        public List<Song> TestData()
+        {
+            return new List<Song>
+                       {
+                           new Song { Album = "Only By The Night", Artist = "Kings of Leon", Name = "Be Somebody" },
+                           new Song { Album = "Oracular Spectacular", Artist = "MGMT", Name = "Kids" },
+                           new Song { Album = "Antics", Artist = "Interpol", Name = "Evil" },
+                           new Song { Album = "Antics", Artist = "Interpol", Name = "Slow Hands" },
+                       };
+        }
+    }
+}
--- src/ITunesIndexer.UnitTests/XNodeExtensionsTests.cs
+++ src/ITunesIndexer.UnitTests/XNodeExtensionsTests.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Xml;
+﻿using System.Xml;
 using System.Xml.Linq;
-using ITunesIndexer.Models;
 using NUnit.Framework;
-using System.Linq;
 
 namespace ITunesIndexer.UnitTests
 {
@@ -16,35 +13,6 @@ namespace ITunesIndexer.UnitTests
             XmlDocument navigable = xNode.ToXmlDocument();
             Assert.IsNotNull(navigable);
             Assert.AreEqual("<test />", navigable.OuterXml);
-        }
-    }
-
-    public class SongSelectorTests
-    {
-        [Test]
-        public void Should_return_1_song_based_on_name()
-        {
-            var librarybuilder = new LibraryBuilder<Song>();
-            var selector = new SongSelector(librarybuilder);
-
-            Song song = selector.FindSingle(x => x.Name == "Come Ray And Come Charles");
-
-            Assert.IsNotNull(song);
-            Assert.AreEqual("Come Ray And Come Charles", song.Name);
-        }
-
-        [Test]
-        public void Should_return_songs_based_on_bitrate()
-        {
-            var librarybuilder = new LibraryBuilder<Song>();
-            var selector = new SongSelector(librarybuilder);
-
-            IEnumerable<Song> songs = selector.FindMany(x => x.BitRate > 40000);
-            IEnumerable<Song> allSongs = librarybuilder.BuildLibrary(ConfigSettings.PathToXml);
-
-            Assert.Greater(1, songs.Count());
-
-            Assert.Less(songs.Count(), allSongs.Count());
         }
     }
 }
