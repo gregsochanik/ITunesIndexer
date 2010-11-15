5f5436b190d2b2f8aa5f392e189ae95209abded4
Greg Sochanik <greg@sochanik.com>
2010-11-15 22:13:39 +0000

    Added test for logging in unit tests

---
+++ src/ITunesIndexer.UnitTests/LoggingTests.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace ITunesIndexer.UnitTests
+{
+    public class LoggingTests
+    {
+        [Test]
+        public void Posting_file_should_log_info_if_success()
+        {
+            var log =  MockRepository.GenerateStub<ILog>();
+
+            log.AssertWasCalled(x => x.Info("Post Added"));
+        }
+
+        [Test]
+        public void Posting_file_should_log_error_if_exception()
+        {
+            var log = MockRepository.GenerateStub<ILog>();
+
+            log.AssertWasCalled(x => x.Error("There was an error"));
+        }
+    }
+}
--- src/ITunesIndexer/SolrPoster.cs
+++ src/ITunesIndexer/SolrPoster.cs
@@ -1,8 +1,17 @@
 ﻿
+using log4net;
+
 namespace ITunesIndexer
 {
     public class SolrPoster<T> : ISolrPoster<T>
     {
+        private readonly ILog _logger;
+
+        public SolrPoster(ILog logger)
+        {
+            _logger = logger;
+        }
+
         // COnvert SOng to XMl
         // cretae http post
         // post song xml to solr
