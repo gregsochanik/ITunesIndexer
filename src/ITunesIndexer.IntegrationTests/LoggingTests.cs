eb0f3889f064cb59e6d6f20e6eb5fc630a4d9629
Greg Sochanik <greg@sochanik.com>
2010-11-15 08:51:02 +0000

    Added failging test for Poster functionality, added log4net

---
+++ src/ITunesIndexer.IntegrationTests/LoggingTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace ITunesIndexer.IntegrationTests
+{
+    public class LoggingTests
+    {
+        [Test]
+        public void SuccessLogLine()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(LoggingTests));
+            log.Info("HELLO WORLD!");
+        }
+
+        [Test]
+        public void Posting_file_should_log_info_if_success()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Posting_file_should_log_error_if_exception()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- src/ITunesIndexer.UnitTests/SolrPosterTests.cs
+++ src/ITunesIndexer.UnitTests/SolrPosterTests.cs
@@ -7,19 +7,21 @@ namespace ITunesIndexer.UnitTests
         [Test]
         public void Item_can_convert_to_vald_xml()
         {
-            Assert.Fail();
+            Assert.Fail("Not Implemented yet");
         }
 
         [Test]
         public void Can_creat_http_post_of_item()
         {
-            Assert.Fail();
+            Assert.Fail("Not Implemented yet");
         }
 
         [Test]
         [Category("Integration")]
         public void Item_is_added_to_solr_instance()
         {
+            Assert.Fail("Not Implemented yet");
         }
     }
+    
 }
