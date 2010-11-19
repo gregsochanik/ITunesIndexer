dc70e9d8485fb83ce40988c1d96ad1521395e938
Greg Sochanik <greg@sochanik.com>
2010-11-19 08:56:20 +0000

    Added new Wrappers for WebRequest/WebResponse to enable mocking to test HttpPoster functionality - still need to transfer serializer over to correct xml schema (eg <field name=''>)

--- src/ITunesIndexer/SolrPoster.cs
+++ src/ITunesIndexer/SolrPoster.cs
@@ -84,6 +84,70 @@ namespace ITunesIndexer
         }
     }
 
+    public interface IWebRequest
+    {
+        Stream GetRequestStream();
+        IWebResponse GetResponse();
+    }
+
+    public class WebRequestWrapper : IWebRequest
+    {
+        private readonly WebRequest _webRequest;
+
+        public string ContentType
+        {
+            get { return _webRequest.ContentType; }
+            set { _webRequest.ContentType = value; }
+        }
+
+        public string Method
+        {
+            get { return _webRequest.Method; }
+            set { _webRequest.Method = value; }
+        }
+
+        public long ContentLength
+        {
+            get { return _webRequest.ContentLength; }
+            set { _webRequest.ContentLength = value; }
+        }
+
+        public WebRequestWrapper(WebRequest webRequest)
+        {
+            _webRequest = webRequest;
+        }
+
+        public Stream GetRequestStream()
+        {
+            return _webRequest.GetRequestStream();
+        }
+
+        public IWebResponse GetResponse()
+        {
+            return new WebResponseWrapper(_webRequest.GetResponse());
+        }
+    }
+
+    public class WebResponseWrapper : IWebResponse
+    {
+        private readonly WebResponse _response;
+
+        public WebResponseWrapper(WebResponse response)
+        {
+            _response = response;
+        }
+
+        public Stream GetResponseStream()
+        {
+            return _response.GetResponseStream();
+        }
+    }
+
+    public interface IWebResponse
+    {
+        Stream GetResponseStream();
+    }
+
     public interface IHttpPoster
     {
         string Post(Uri location, string parameters);
